d78f88b97e0803cc6edbf1835683ce98c79ae3b1
GwonYong Lee <dlrnjsdyd777@naver.com>
2022-09-29 21:07:49 +0900

    9/29 배경화면 만들고 끝 캐릭터 좌표 찾아야 함

---
+++ sprites/Tekken_Imitation.py
@@ -0,0 +1,22 @@
+from pico2d import *
+Screen_width,Screen_Height = 1200,800
+
+open_canvas(1200,800)
+BackGround1 = load_image('BackGrounds1.png')
+Jin = load_image('Jin.png')
+
+running = True
+x,y=200,200
+frame = 0
+
+while running:
+  clear_canvas()
+  BackGround1.draw(Screen_width // 2 + 1,Screen_Height // 2 + 1)
+  Jin.clip_draw(frame*100,2560,50,140,x,y)
+  frame = (frame+1) % 8
+  update_canvas()
+
+
+  delay(0.1)
+
+close_canvas()
