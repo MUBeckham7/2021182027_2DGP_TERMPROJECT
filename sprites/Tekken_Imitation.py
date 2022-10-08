6b4ac8436ac58f834c94d9342eef714808cdce3c
MUBeckham7 <112480058+MUBeckham7@users.noreply.github.com>
2022-10-08 14:44:16 +0900

    펀치 구현중

--- sprites/Tekken_Imitation.py
+++ sprites/Tekken_Imitation.py
@@ -1,7 +1,7 @@
 from pico2d import *
 Screen_width,Screen_Height = 1200,800
 
-open_canvas(1200,800)
+open_canvas(800,600)
 BackGround1 = load_image('BackGrounds1.png')
 Jin = load_image('Jin.png')
 
@@ -9,12 +9,13 @@ running = True
 x,y=200,200
 frame = 0
 dir_x=0
-
+space = 0
 #키보드 입력 함수
 def handle_events():
   global running
   global x,y
   global dir_x
+  global space
   events = get_events()
   for event in events:
     if event.type == SDL_QUIT:
@@ -22,16 +23,19 @@ def handle_events():
     elif event.type == SDL_KEYDOWN:
       if event.key == SDLK_ESCAPE:
         running = False
-      elif event.key == SDLK_RIGHT:
+      elif event.key == SDLK_d:
         dir_x += 1
-      elif event.key== SDLK_LEFT:
+      elif event.key == SDLK_a:
         dir_x -= 1
+      elif event.key == SDLK_SPACE:
+        space = 1
     elif event.type == SDL_KEYUP:
-      if event.key == SDLK_RIGHT:
+      if event.key == SDLK_d:
         dir_x -= 1
-      elif event.key== SDLK_LEFT:
+      elif event.key == SDLK_a:
         dir_x += 1
-
+      elif event.key == SDLK_SPACE:
+        space = 0
 
 
 while running:
@@ -42,7 +46,11 @@ while running:
   x += dir_x * 10
   frame = (frame+1) % 4
 
-  Jin.clip_draw(frame*160,2580,200,280,x,y)
+  if space == 0:
+    Jin.clip_draw(frame*160,2580,100,140,x,y)
+  elif space == 1:
+    Jin.clip_draw(frame*160,1150,100,280,x,y)
+
   update_canvas()
 
 
