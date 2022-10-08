f265454b5cca75adc4f7f57245e620cd9207b751
MUBeckham7 <112480058+MUBeckham7@users.noreply.github.com>
2022-10-08 11:30:26 +0900

    첫번째 객체 오른쪽 이동 구현

--- sprites/Tekken_Imitation.py
+++ sprites/Tekken_Imitation.py
@@ -8,13 +8,43 @@ Jin = load_image('Jin.png')
 running = True
 x,y=200,200
 frame = 0
+dir_x=0
+
+#키보드 입력 함수
+def handle_events():
+  global running
+  global x,y
+  global dir_x
+  events = get_events()
+  for event in events:
+    if event.type == SDL_QUIT:
+      running = False
+    elif event.type == SDL_KEYDOWN:
+      if event.key == SDLK_ESCAPE:
+        running = False
+      elif event.key == SDLK_RIGHT:
+        dir_x += 1
+
+    elif event.type == SDL_KEYUP:
+      if event.key == SDLK_RIGHT:
+        dir_x -= 1
+
+
+
 
 while running:
   clear_canvas()
+
+  handle_events()
   BackGround1.draw(Screen_width // 2 + 1,Screen_Height // 2 + 1)
-  Jin.clip_draw(frame*100,0,50,140,x,y)
-  frame = (frame+1) % 8
+  x += dir_x * 5
+  frame = (frame+1) % 4
+
+  Jin.clip_draw(frame*160,2580,200,320,x,y)
   update_canvas()
+
+
+
 
 
   delay(0.1)
