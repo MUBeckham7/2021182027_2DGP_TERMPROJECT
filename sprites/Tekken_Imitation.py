dd1599c6af65baf6b20cf05060690f7b88bf6697
MUBeckham7 <112480058+MUBeckham7@users.noreply.github.com>
2022-10-09 15:44:33 +0900

    Update Tekken_Imitation.py

--- sprites/Tekken_Imitation.py
+++ sprites/Tekken_Imitation.py
@@ -8,8 +8,9 @@ Jin = load_image('Jin.png')
 running = True
 x,y=200,200
 frame = 0
+punch = 0
 dir_x=0
-space = 0
+space = False
 #키보드 입력 함수
 def handle_events():
   global running
@@ -28,14 +29,14 @@ def handle_events():
       elif event.key == SDLK_a:
         dir_x -= 1
       elif event.key == SDLK_SPACE:
-        space = 1
+        space = True
     elif event.type == SDL_KEYUP:
       if event.key == SDLK_d:
         dir_x -= 1
       elif event.key == SDLK_a:
         dir_x += 1
       elif event.key == SDLK_SPACE:
-        space = 0
+        space = False
 
 
 while running:
@@ -45,11 +46,12 @@ while running:
   BackGround1.draw(Screen_width // 2 + 1,Screen_Height // 2 + 1)
   x += dir_x * 10
   frame = (frame+1) % 4
+  punch = (punch+1) % 2
 
-  if space == 0:
+  if space == False:
     Jin.clip_draw(frame*160,2580,100,140,x,y)
-  elif space == 1:
-    Jin.clip_draw(frame*160,1150,100,280,x,y)
+  elif space == True:
+    Jin.clip_draw(punch*160, 1230, 120, 140, x, y)
 
   update_canvas()
 
