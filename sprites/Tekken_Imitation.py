21b842c4ab365d16b0b5d07c34481739331d7d34
GwonYong Lee <dlrnjsdyd777@naver.com>
2022-10-11 21:26:01 +0900

    오류 수정 및 두번째 객체 구현 중

--- sprites/Tekken_Imitation.py
+++ sprites/Tekken_Imitation.py
@@ -4,22 +4,29 @@ Screen_width,Screen_Height = 1200,800
 open_canvas(800,600)
 BackGround1 = load_image('BackGrounds1.png')
 Jin = load_image('Jin.png')
+kazuya = load_image('Kazuya.png')
 
 running = True
 x,y=200,200
+x2,y2=600,200
 frame_1 = 0
+frame_2 = 0
 kick_1=0
 punch_1 = 0
-dir_x=0
+dir_x_1=0
+dir_x_2=0
 space = False
 k = False
+defence_1 = False
 #키보드 입력 함수
 def handle_events():
   global running
   global x,y
-  global dir_x
+  global dir_x_1
+  global dir_x_2
   global space
   global k
+  global defence_1
   events = get_events()
   for event in events:
     if event.type == SDL_QUIT:
@@ -28,18 +35,28 @@ def handle_events():
       if event.key == SDLK_ESCAPE:
         running = False
       elif event.key == SDLK_d:
-        dir_x += 1
+        dir_x_1 += 1
       elif event.key == SDLK_a:
-        dir_x -= 0.5
+        dir_x_1 -= 0.5
+        defence_1 = True
       elif event.key == SDLK_SPACE:
         space = True
       elif event.key == SDLK_k:
         k = True
+      elif event.key == SDLK_LEFT:   #두 번째 객체
+        dir_x_2 -= 1
+      elif event.key == SDLK_RIGHT:
+        dir_x_2 +=0.6
     elif event.type == SDL_KEYUP:
       if event.key == SDLK_d:
-        dir_x -= 1
+        dir_x_1 -= 1
       elif event.key == SDLK_a:
-        dir_x += 0.5
+        dir_x_1 += 0.5
+        defence_1 = False
+      elif event.key == SDLK_LEFT:
+        dir_x_2 += 1
+      elif event.key == SDLK_RIGHT:
+        dir_x_2 -= 0.6
 
 
 while running:
@@ -47,14 +64,17 @@ while running:
 
   handle_events()
   BackGround1.draw(Screen_width // 2 + 1,Screen_Height // 2 + 1)
-  x += dir_x * 10
+  x += dir_x_1 * 10
+  x2 += dir_x_2 * 10
+
   frame_1 = (frame_1 + 1) % 4
+  frame_2 = (frame_2 + 1) % 6
 
 
 
 
 
-  if space == False and k == False:
+  if space == False and k == False and defence_1 == False:
     Jin.clip_draw(frame_1 * 160, 2580, 100, 140, x, y)
   elif space == True:
     Jin.clip_draw(punch_1*170, 1220, 115, 140, x, y)
@@ -66,8 +86,10 @@ while running:
     kick_1 = (kick_1 + 1) % 5
     if kick_1 == 4:
       k=False
+  elif space == False and k == False and defence_1 == True:
+    Jin.clip_draw(640, 80, 130, 100, x, y)
 
-
+  kazuya.clip_draw(frame_2 * 142, 2520, 120, 140, x2, y2)
 
 
 
