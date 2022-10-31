1b6127e40fbb539f33186277bacaa87e72bd53e7
GwonYong Lee <dlrnjsdyd777@naver.com>
2022-10-31 21:22:36 +0900

    프레임 워크로 타이틀 스테이트 구현완료

--- sprites/background.py
+++
@@ -1,9 +0,0 @@
-from pico2d import *
-Screen_width,Screen_Height = 1200,800
-
-class BackGround:
-    def __init__(self):
-        self.image = load_image('BackGrounds1.png')
-
-    def draw(self):
-        self.image.draw(Screen_width // 2 + 1, Screen_Height // 2 + 1)
--- sprites/title_state.py
+++ sprites/title_state.py
@@ -1,49 +1,36 @@
 from pico2d import *
 import game_framework
-from title import Title
-title = None
-import game_state
+import Tekken_Imitation
+
+image = None
+
+def enter():
+    global image
+    image = load_image('title_image.png')
+
+def exit():
+    global image
+    del image
 
 def handle_events():
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
-        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
-            game_framework.quit()
-        elif (event.type, event.key) == (SDL_KEYDOWN,SDLK_RETURN):
-            game_framework.change_state(game_state)
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_q:
+            game_framework.change_state(Tekken_Imitation)
 
-def enter():
-    global title
-    title = Title()
-
-def exit():
-    global title
-    del title
-
-def update():
-    pass
-
-def draw_world():
-    title.draw()
 
 def draw():
     clear_canvas()
-    draw_world()
+    image.draw(400,300)
     update_canvas()
+
+def update():
+    pass
 
 def pause():
     pass
 
 def resume():
     pass
-def test_self():
-    import title_state
-
-    pico2d.open_canvas()
-    game_framework.run(title_state)
-    pico2d.clear_canvas()
-
-if __name__ == '__main__':
-    test_self()
