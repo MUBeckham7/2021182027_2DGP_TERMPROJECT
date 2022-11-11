5271070f894357a19d5c03b26d04b3055c8dece6
MUBeckham7 <112480058+MUBeckham7@users.noreply.github.com>
2022-11-11 20:04:31 +0900

    체력바 애니메이션 구현중

--- sprites/rightlifebar.py
+++ sprites/rightlifebar.py
@@ -7,5 +7,7 @@ class RightLifeBar:
     def update(self):
         pass
 
+    #기본 613,325
     def draw(self):
+        self.image.clip_draw_to_origin(0,0,460,50,613,515,100,80)
         self.image.clip_composite_draw(0,0,460,50,0,'',613,515,325,38)
