bf952c0916b2e1dead8c1bcefae976930d1dd657
MUBeckham7 <112480058+MUBeckham7@users.noreply.github.com>
2022-11-12 16:04:13 +0900

    게임 화면내에 배경음악 추가완료

--- sprites/background.py
+++ sprites/background.py
@@ -4,6 +4,9 @@ Screen_width,Screen_Height = 1200,800
 class BackGround:
     def __init__(self):
         self.image = load_image('BackGrounds1.png')
+        self.bgm = load_music('game_state_sound.mp3')
+        self.bgm.set_volume(15)
+        self.bgm.repeat_play()
 
     def update(self):
         pass
@@ -13,3 +16,4 @@ class BackGround:
 
     def get_bb(self):
         return 0,0,800,600
+
